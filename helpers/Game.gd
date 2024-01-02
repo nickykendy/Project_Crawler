93447bcb2438307420c7f053be6981228b635d64
NickyKendy Hua <https://gitee.com/nickykendyhua>
2024-01-02 19:26:19 +0800

    初始化项目

--- helpers/Game.gd
+++ helpers/Game.gd
@@ -1,2 +1,18 @@
 extends Node
 
+
+const TILESIZE := 32
+const TILE_NONE := Vector2i(0, 0)
+const TILE_FLOOR := Vector2i(1, 0)
+const TILE_WALL := Vector2i(2, 0)
+const TILE_STAIRS := Vector2i(1, 1)
+const TILE_DOOR := Vector2i(0, 1)
+const TILE_FOG := Vector2i(2, 1)
+const TILE_DARK := Vector2i(3, 1)
+const TILE_VOID := Vector2i(3, 0)
+
+var level_size := Vector2i(26, 25)
+
+func _ready():
+	DisplayServer.window_set_size(Vector2(1280, 800), 0)
+	randomize()
---
+++ scripts/hero.gd
@@ -0,0 +1,83 @@
+extends Unit
+class_name Hero
+
+
+var is_hero_turn := true
+var fov_range := 6
+
+
+func _ready():
+	_initialization()
+
+
+func _input(event):
+	if !event.is_pressed():
+		return
+	
+	if is_hero_turn:
+		if event.is_action_pressed("left"):
+			act(-1, 0)
+		elif event.is_action_pressed("right"):
+			act(1, 0)
+		elif event.is_action_pressed("up"):
+			act(0, -1)
+		elif event.is_action_pressed("down"):
+			act(0, 1)
+		elif event.is_action_pressed("wait"):
+			act(0, 0)
+
+
+func _unhandled_input(event):
+	if event.is_action_released("scale up"):
+		$Camera2D.zoom += Vector2(0.1, 0.1)
+	elif event. is_action_released("scale down"):
+		$Camera2D.zoom -= Vector2(0.1, 0.1)
+	$Camera2D.zoom = clamp($Camera2D.zoom, Vector2(0.5, 0.5), Vector2(2.0, 2.0))
+
+
+func act(dx:int, dy:int) -> void:
+	var tile_map :TileMap = get_parent().get_node("TileMap")
+	if tile_map == null: return
+	var world = get_parent()
+	if world == null: return
+	
+	var _x := current_tile.x + dx
+	var _y := current_tile.y + dy
+	
+	var dest := Vector2i(_x, _y)
+	var tile := tile_map.get_cell_atlas_coords(0, dest)
+	var is_open_door := false
+	var is_wait_range_hit := false
+	
+	# 尝试移动时，遇敌发起攻击
+	if tile == Game.TILE_FLOOR:
+		var blocked = false
+		if !world.monsters.is_empty():
+			for mon in world.monsters:
+				var mon_pos = mon.current_tile
+				if mon_pos == dest:
+					# TODO 敌人受击逻辑
+					
+					if mon.dead:
+						world.monsters.erase(mon)
+					blocked = true
+					break
+					
+		if !blocked:
+			current_tile = dest
+	# 尝试打开门
+	elif tile == Game.TILE_DOOR:
+		tile_map.set_cell(0, dest, 0, Game.TILE_FLOOR)
+		is_open_door = true
+	
+	try_act.emit(dest, is_open_door)
+	position = current_tile * Game.TILESIZE
+	
+	if !is_wait_range_hit:
+		acted.emit()
+	
+	is_hero_turn = false
+
+
+func _process(_delta):
+	pass
